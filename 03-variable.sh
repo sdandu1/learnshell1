bf8892b7b21d7cef66ce7233c83a3406fec760f5
sdandu <dsraju15@gmail.com>
2023-06-03 23:14:07 +0530

    11

--- 03-variable.sh
+++ 03-variable.sh
@@ -12,3 +12,12 @@ Student=srinivas
 echo Trainer name $Trainer
 echo Batch number $Batch
 echo Student name ${Student}
+
+
+#Hard coded the date and time
+date0=june-03-2023
+echo Today date is $date0
+
+#Dynamic date change
+DATE1=$(date)
+echo hey, Today date is ${DATE1}

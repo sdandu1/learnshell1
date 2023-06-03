a8814f2d3220d0de9c1a0564db41a115a81d109b
sdandu <dsraju15@gmail.com>
2023-06-03 23:08:03 +0530

    11

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,14 +1,14 @@
 #In the bash sheel we Decalring variable as
 a=30
 b=20
-$a
-${b}
+echo value a is $a
+echo value b is ${b}
 
 #In the bash shell we access the variable as
 Trainer=Raghu
 Batch=70
 Student=srinivas
 
-$Trainer
-$Batch
-${Student}
+echo Trainer name $Trainer
+echo Batch number $Batch
+echo Student name ${Student}

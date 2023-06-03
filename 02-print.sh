6f2f0d862763ea894b9462803f0c2fe30c03b2ab
sdandu <dsraju15@gmail.com>
2023-06-03 22:40:52 +0530

    1

--- 02-print.sh
+++ 02-print.sh
@@ -1,9 +1,11 @@
 echo Hello world
 echo "**** Hello World ****"
 
-echo -e "\e[1;31m This is red text \e[0m"
+echo -e "\e[31m This is red text \e[0m"
 echo -e "\e[2;32m This is Green text\e[0m"
 echo -e "\e[3;33m This is Yellow text\e[0m"
 echo -e "\e[3;34m This is Blue text\e[0m"
 echo -e "\e[5;35m This is Magenta text\e[0m"
 echo -e "\e[6 36m This is Clan text\e[0m"
+
+#echo -e "\e"

4369eb7c811334bf19e1c36196a144d87d39cc01
sdandu <dsraju15@gmail.com>
2023-07-10 16:36:41 +0530

    111

---
+++ 07-special-var.sh
@@ -0,0 +1,17 @@
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+
+example() {
+           echo "Following values from function"
+           echo "0 - $0"
+           echo "1 - $1"
+           echo "2 - $2"
+           echo "* - $*"
+           echo "@ - $@"
+           echo "# - $#"
+           }
+example xyz 20

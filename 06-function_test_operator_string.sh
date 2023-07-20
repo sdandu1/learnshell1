792fd47b91c2fab98d6b5875f5e687c344420dd0
sdandu <dsraju15@gmail.com>
2023-07-20 16:47:16 +0530

    1111

--- 06-function_test_operator_string.sh
+++ 06-function_test_operator_string.sh
@@ -1,17 +1,52 @@
 #! /bin/bash
 
+#String tests:
+#string1 = string2: True if string1 is equal to string2.
+#string1 != string2: True if string1 is not equal to string2.
+#-z string: True if string is empty (zero length).
+#-n string: True if string is not empty.
+
 #Examples in String
 String1="hello"
-String2="hello"
+String2="world"
 String3=""
 
 #Test if two strings are equal
 string_comp()
 if ["$string1" = "$string2"]; then
-  echo -e "\e[1;32m 1st string $String1 and 2nd string $String2 are eqaul \e[0m"
+  echo -e "\e[1;31m 1st string $String1 and 2nd string $String2 are eqaul \e[0m"
   else
   echo -e "\e[1;32m 1st string $String1 and 2nd string $String2 are not eqaul \e[0m"
 fi
 
+string_comp1()
+if ["$String1" != "$String2"]; then
+  echo -e "\e[1;31m These two strings are not equal\e[0m"
+  else
+  echo -e "\e[1;32m These two strings are equal\e[0m"
+if
+
+#check if the sting is empty
+string_comp2()
+if [-z "$string3" ]; then
+  echo -e "\e[1;31 The string is empty\e[0m"
+  else
+  echo -e "\e[1;32 The string is not empty\e[0m"
+fi
+
+#check if the sting is not empty
+string_comp3()
+if [-n "$string3"]; then
+  echo -e "\e[1;32 The string is not empty\e[0m"
+  else
+  echo -e "\e[1;31 The string is empty\e[0m"
+if
+
+
+#calling function
 string_comp
+string_comp1
+string_comp2
+string_comp3
+
 

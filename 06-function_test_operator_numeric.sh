3fa0a4b4143d6fae90d7f33a2a96bd548c200052
sdandu <dsraju15@gmail.com>
2023-07-20 20:27:34 +0530

    1111

--- 06-function_test_operator_numeric.sh
+++ 06-function_test_operator_numeric.sh
@@ -4,7 +4,7 @@ num1=10
 num2=20
 
 #Test if two number are eqaul
-function f1() {
+function1() {
 
 if [ "$num1" -eq "$num2"  ]; then
   echo "These two number are equal"
@@ -15,7 +15,7 @@ fi
 }
 
 # Test if num1 is greater than num2
-function f2() {
+function2() {
     if [ "$num1" -gt "$num2" ]; then
       echo "$num1 is not greater $num2"
       else
@@ -26,7 +26,7 @@ function f2() {
 
 
 # Test if num1 is greater than or equal to num2
-function f3() {
+function3() {
   if [ "$num1" -ge "$num2" ]; then
     echo "$num1 is not equal and not greaterthan $num2"
     echo "$num1 is  equal and greaterthan $num2"
@@ -35,7 +35,7 @@ function f3() {
     
 }
 # Test if num1 is greater than or equal to num2
-function f4() {
+function4() {
   if [ "$num1" -lt "$num2" ]; then
     echo "$num1 is not equal and not greaterthan $num2"
     echo "$num1 is  equal and greaterthan $num2"
@@ -46,9 +46,9 @@ function f4() {
 
 #calling functions.
 
-function f1
-function f2
-function f3
-function f4
+function1
+function2
+function3
+function4
 
 
